fdd4b8eca2aec6156a3a2bddde4576da067066f8
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-10-17 20:33:07 -0400

    10-17-24 8-kyu

---
+++ 10-17-24-feast-of-beasts-8-kyu.js
@@ -0,0 +1,22 @@
+// The Feast of Many Beasts
+// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+
+function feast(beast, dish) {
+  //your function here
+  if(beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]){
+    return true
+  }
+  return false
+}
+
+
+
+
+
+
+console.log(feast("great blue heron", "garlic naan"), true)
+console.log(feast("chickadee", "chocolate cake"), true)
+console.log(feast("brown bear", "bear claw"), false)
